a49dedc992bcba670d35d578f830804512808fd6
Gus Reiber <67433385+gusreiber-nsd@users.noreply.github.com>
2024-07-31 22:46:59 -0700

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, '../public')));
+// Use the CORS middleware
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+// Handle undefined routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// Catch uncaught exceptions
+process.on('uncaughtException', (err) => {
+    console.error('There was an uncaught error', err);
+    process.exit(1); // mandatory (as per the Node.js docs)
+});
